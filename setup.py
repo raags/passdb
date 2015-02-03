8f213dea87e2c234aaa6a03ba95030142b058042
Raghu Udiyar <raghu@helpshift.com>
2015-02-03 16:33:48 +0530

    Fixes for Pypi

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@
 
 from setuptools import setup
 
-setup(name='pdb',
-      version='0.1.0',
+setup(
+      name='pdb',
+      packages=['pdb'],
+      version='0.1',
       description='PDB (Password DB) for shared password management',
       author='Raghu Udiyar',
       author_email='raghusiddarth@gmail.com',
       url='https://github.com/raags/pdb',
+      download_url = 'https://github.com/raags/pdb/tarball/0.1',
       install_requires=['python-gnupg', 'argparse', 'pyyaml'],
-      packages=['pdb'],
       entry_points = {
         'console_scripts': [
         'pdb=pdb.pdb:main',
